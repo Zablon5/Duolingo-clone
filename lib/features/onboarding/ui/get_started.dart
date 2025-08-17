335daa426645f35eb3986d8241f238eb6065b697
Zerihun5 <zgirma74@gmail.com>
2025-08-17 23:48:38 +0300

    feat: add get started page and make the splash to navigate to it 3 sec later

---
+++ lib/features/onboarding/ui/get_started.dart
@@ -0,0 +1,73 @@
+import 'package:chiclet/chiclet.dart';
+import 'package:duolingo_clone/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.darkBlue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height * 0.3),
+            Container(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Duolingo Icon Animation Placeholder',
+                style: TextStyle(color: AppColors.light),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'duolingo',
+                style: TextStyle(color: AppColors.primaryGreen),
+              ),
+            ),
+            Text(
+              'The free, fun, and effective way to \nlearn a language!',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: AppColors.dark, fontFamily: 'DIN'),
+            ),
+
+            SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ChicletAnimatedButton(
+                backgroundColor: AppColors.secondaryGreen,
+                onPressed: () {
+                  // TODO:Handle button press
+                },
+                width: double.infinity,
+                child: Text(
+                  'GET STARTED',
+                  style: TextStyle(color: AppColors.darkBlue),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ChicletOutlinedAnimatedButton(
+                onPressed: () {
+                  // TODO:Handle button press
+                },
+                width: double.infinity,
+                backgroundColor: AppColors.darkBlue,
+                child: Text(
+                  'I ALREADY HAVE AN ACCOUNT',
+                  style: TextStyle(color: AppColors.secondaryGreen),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/splash/ui/screens/splash_screen.dart
+++ lib/features/splash/ui/screens/splash_screen.dart
@@ -1,9 +1,29 @@
+import 'dart:async';
+
+import 'package:duolingo_clone/features/onboarding/ui/get_started.dart';
 import 'package:duolingo_clone/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => GetStartedPage()),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     const String assetName = 'logo/logotype.svg';
